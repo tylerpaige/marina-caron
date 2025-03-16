3c484502ea052502d37a9a3f5b30fd32466fb703
Tyler Paige <tylerclarkpaige@gmail.com>
2025-03-16 11:57:37 -0400

    Clear out console logs

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -6,12 +6,10 @@ import { revalidatePath } from 'next/cache';
 
 export async function POST(request) {
   try {
-    console.log('Revalidating')
     const signature = request.headers.get(SIGNATURE_HEADER_NAME)
     const body = await request.text()
-    console.log({body, signature, secret})
     const validity = await isValidSignature(body, signature, secret)
-    console.log({validity})
+
     if (!validity) {
       return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
     }
